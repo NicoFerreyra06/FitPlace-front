8165878ca25dd1623319f61da97ff53479210587
nicolasferreyramdp-code <nicolasferreyramdp@gmail.com>
2026-07-21 15:15:41 -0300

    fix proxy para vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://fitplace.onrender.com',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
